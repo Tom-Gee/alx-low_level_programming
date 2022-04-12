3701c8b6613598b764a57d43a1aa73e90a1ce986
tom-gee <tomgeorge.kingdom@gmail.com>
2022-04-11 17:44:05 -0700

    I love

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <studio.h>
+
+/**
+ * main - print if insomnia and fall asleep
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	putchar('\n');
+	return (0);
+}
