0a5dec286d794070ebae0e2fa0c5df8751b4b380
tom-gee <tomgeorge.kingdom@gmail.com>
2022-05-10 16:39:42 -0700

    0x0C

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,7 +32,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		j = n;
 	s = malloc(sizeof(char) * (i + j + 1));
 	if (s == NULL)
-		return (NULL)
+		return (NULL);
 	for (k = 0; k < i; k++)
 		s[k] = s1[k];
 	for (k = 0; k < j; k++)
